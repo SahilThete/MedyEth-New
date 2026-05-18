d0476b1a56167f02ac10c9c1182af56b1b83a4ed
Sahil Thete <sahilthete.st@gmail.com>
2026-05-18 15:45:11 +0530

    removed unused files/routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { MetaMaskProvider } from "@metamask/sdk-react";
-// import dotenv from "dotenv";
-// dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
